e57d0222b714a2179b525332c346c5bb9301ba72
Nikita <01123niko@gmil.com>
2026-04-19 23:45:35 +0300

    fix mobile page layout

--- server.js
+++ server.js
@@ -77,6 +77,10 @@ app.get("/days", async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
